ff1a013cce559daed3065e1b35ff41b8f97e6440
RishirajSrkr <113599778+RishirajSrkr@users.noreply.github.com>
2024-07-23 00:34:15 +0530

    db

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -4,6 +4,7 @@ async function dbConnection(){
     try{
         // await mongoose.connect(process.env.DB_URL)
         await mongoose.connect(process.env.DB_URL);
+        console.log("Connected to database.");
     }
     catch(error){
         console.log(`Error while connecting to mongodb :: ${error}`)
